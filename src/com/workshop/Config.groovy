b28a6950698aa67b0d926d4469c67ae093a49af3
Made Aryadinata <made.aryadinata2@btpn.com>
2021-01-23 15:32:28 +0700

    add default_docker_registry

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -4,6 +4,7 @@ package com.workshop
 class Config {
    // Docker related default variable
    def default_docker_jenkins_tool = 'docker'
+   def default_docker_registry = "https://registry-1.docker.io/v2/"
 
    // Golang related default variable
    def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
